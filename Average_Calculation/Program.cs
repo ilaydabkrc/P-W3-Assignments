16f2975ec1727d3e00c910e2378ef710d46fec7c
İlayda Bakırcıoğlu <119436349+ilaydabkrc@users.noreply.github.com>
2025-05-25 16:03:01 +0300

    Average Calculation

---
+++ Average_Calculation/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        //Get the Grade
+        Console.WriteLine("Birinci ders notunu giriniz: ");
+        double grade1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("İkinci ders notunu giriniz: ");
+        double grade2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Üçüncü ders notunu giriniz: ");
+        double grade3 = Convert.ToDouble(Console.ReadLine());
+
+        //Valida grades
+        if (!IsValidGrade(grade1) || !IsValidGrade(grade2) || !IsValidGrade(grade3))
+        {
+            Console.WriteLine("Hata: Notlar 0-100 arasında değil.");
+            return;
+        }
+        //Calculate average
+        double average = (grade1 + grade2 + grade3) / 3;
+        //Determine letter grade
+        string letterGrade = GetLetterGrade(average);
+
+        //Show result
+        Console.WriteLine($"\nOrtalamanız : {average}");
+        Console.WriteLine($"\nHarf notunuz : {letterGrade}");
+    }
+
+    // Grade validity check
+    static bool IsValidGrade(double grade)
+    {
+        return grade >= 0 && grade <= 100;
+    }
+    //Determine letter grade based an average
+    static string GetLetterGrade(double average)
+    {
+        if (average >= 90 && average <= 100) return "AA";
+        else if (average >= 85) return "BA";
+        else if (average >= 80) return "BB";
+        else if (average >= 75) return "CB";
+        else if (average >= 70) return "CC";
+        else if (average >= 65) return "DC";
+        else if (average >= 60) return "DD";
+        else if (average >= 55) return "FD";
+        else return "FF";
+    }
+
+
+
+}
