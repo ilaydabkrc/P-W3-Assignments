fc7c6bc6d36fe713d19aa11bcfed724773859953
İlayda Bakırcıoğlu <119436349+ilaydabkrc@users.noreply.github.com>
2025-05-25 16:05:46 +0300

    Random Number Guess

---
+++ Random_Number_Guess/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int secretNumber = random.Next(1, 100);
+        int lives = 5;
+        bool isGuessedCorrectly = false;
+
+        Console.WriteLine("RASTGELE SAYI BULMA OYUNU");
+        Console.WriteLine("Rastgele 1-100 arası bir sayı giriniz");
+        Console.WriteLine("Toplam 5 adet tahmin hakkınız var.");
+
+        while (lives > 0)
+        {
+            Console.WriteLine("Tahmininiz: ");
+            string input = Console.ReadLine();
+            int guess;
+
+            if (!int.TryParse(input, out guess))
+            {
+                Console.WriteLine("Lütfen geçerli bir sayı giriniz.");
+                continue;
+            }
+
+            if (guess < 1)
+            {
+                Console.WriteLine("Lütfen 1 ile 100 arasında bir sayı giriniz.");
+                continue;
+            }
+
+            if (guess == secretNumber)
+            {
+                Console.WriteLine("Tebrikler! Doğru tahmin ettiniz.");
+                isGuessedCorrectly=true;
+                break;
+            }
+            else if(guess < secretNumber)
+            {
+                Console.WriteLine("Daha büyük bir sayı giriniz.");
+
+            }
+            else
+            {
+                Console.WriteLine("Daha düşük bir sayı giriniz. ");
+            }
+            lives--;
+            Console.WriteLine("Kalan hakkınız: " + lives + "\n");
+         }
+        if (!isGuessedCorrectly)
+        {
+            Console.WriteLine($"Tahmin hakkınız bitti. Doğru sayı: {secretNumber}");
+        }
+        Console.WriteLine("\nOyun bitti çıkmak için bir tuşa basınız.");
+        Console.ReadKey();
+
+    }
+
+
+
+
+
+
+
+
+
+}
